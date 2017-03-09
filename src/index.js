1fac6550c2d7718a268352539c0b3c404833cb64
colon <colon.song@gmail.com>
2017-03-09 17:35:56 +0800

    拼錯

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /**
  * Created by colon on 2017/3/9.
  */
+var messages = require('./messages');
+
 var app = document.getElementById('app');
-app.innerHTML = '<p> hello </p>';
+app.innerHTML = '<p> ' + messages.hi + ' ' + messages.event + '</p>';
