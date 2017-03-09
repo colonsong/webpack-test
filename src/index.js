9ca1a17fb2619015109e8d3bf89b12ccb79ae763
colon <colon.song@gmail.com>
2017-03-09 22:21:19 +0800

    Unexpected token: punc ()) UglifyJS error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-/**
- * Created by colon on 2017/3/9.
- */
-var messages = require('./messages');
+import {multiply} from './mathStuff';
 
-import Button from './button';
-import CarImg from './image';
-
-console.log(Button);
-
-var newMessage = () => (`<p>${messages.hi} 
-${CarImg}
-${messages.event}</p>`);
-
+const newMessage =  () => (multiply(3,3));
 
 var app = document.getElementById('app');
 app.innerHTML = newMessage();
